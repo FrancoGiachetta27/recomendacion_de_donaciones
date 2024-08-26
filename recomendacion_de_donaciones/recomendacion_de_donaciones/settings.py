495b900156cb9d72332493da75203628167f2468
Franco Giachetta <fgiachetta@frba.utn.edu.ar>
2024-08-25 22:34:52 -0300

    agrego variables de entorno

--- recomendacion_de_donaciones/recomendacion_de_donaciones/settings.py
+++ recomendacion_de_donaciones/recomendacion_de_donaciones/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,11 +80,11 @@ WSGI_APPLICATION = 'recomendacion_de_donaciones.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'dds_database',
+        'NAME': os.getenv('DB_NAME'),
         'USER': 'root',
-        'PASSWORD': '<mysql_pass>',
+        'PASSWORD': os.getenv('PASSWORD'),
         'HOST': 'localhost',
-        'PORT': 3306
+        'PORT': os.getenv('PORT')
     }
 }
 
