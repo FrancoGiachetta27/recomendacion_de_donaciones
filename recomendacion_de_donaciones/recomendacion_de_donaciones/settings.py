b085f66f92af76a0e25ae70aea856fc05c31f006
Franco Giachetta <fgiachetta@frba.utn.edu.ar>
2024-09-12 19:02:40 -0300

    fix static files

--- recomendacion_de_donaciones/recomendacion_de_donaciones/settings.py
+++ recomendacion_de_donaciones/recomendacion_de_donaciones/settings.py
@@ -44,7 +44,6 @@ else:
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "recomendaciones-donaciones.up.railway.app"]
 CSRF_TRUSTED_ORIGINS = ["https://recomendaciones-donaciones.up.railway.app"]
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Application definition
